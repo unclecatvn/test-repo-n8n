9e65feb849d101d848eec27a305612135e1d10b7
HoaiNam <nguyenhoain123@gmail.com>
2025-10-20 01:44:23 +0700

    New code

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("Hello, World!")
+column_names = ['id', 'color', 'style']
+column_values = [1, 'red', 'bold']
+
+# Convert lists to a dictionary
+name_to_value_dict = dict(zip(column_names, column_values))
+
+print(name_to_value_dict)
+# Output: {'id': 1, 'color': 'red', 'style': 'bold'}
--- test1.py
+++ test1.py
@@ -8,7 +8,7 @@ b = 5
 c = 6
 
 # calculate the discriminant
-d = (b**2) - (4*a*c)
+d = (b**2) - (4*a*c) + (2*b)
 
 # find two solutions
 sol1 = (-b-cmath.sqrt(d))/(2*a)
